6d4ea666204fea593049e79d39d57d9925b3b4e9
David Lau <david.kaimun.lau@gmail.com>
2023-12-12 19:05:29 +0100

    add models and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'Cleaning database...'
+Booking.delete_all
+Reservation.delete_all
+Listing.delete_all
+
+puts 'Creating listings...'
+ap Listing.create!(num_rooms: 2)
+ap Listing.create!(num_rooms: 1)
+ap Listing.create!(num_rooms: 3)
+
+puts 'Creating bookings...'
+ap Booking.create!(listing_id: Listing.first.id, start_date: '2016-10-10'.to_date, end_date: '2016-10-15'.to_date)
+ap Booking.create!(listing_id: Listing.first.id, start_date: '2016-10-16'.to_date, end_date: '2016-10-20'.to_date)
+ap Booking.create!(listing_id: Listing.second.id, start_date: '2016-10-15'.to_date, end_date: '2016-10-20'.to_date)
+
+puts 'Creating reservations...'
+ap Reservation.create!(listing_id: Listing.first.id, start_date: '2016-10-11'.to_date, end_date: '2016-10-13'.to_date)
+ap Reservation.create!(listing_id: Listing.first.id, start_date: '2016-10-13'.to_date, end_date: '2016-10-15'.to_date)
+ap Reservation.create!(listing_id: Listing.first.id, start_date: '2016-10-16'.to_date, end_date: '2016-10-20'.to_date)
+ap Reservation.create!(listing_id: Listing.second.id, start_date: '2016-10-15'.to_date, end_date: '2016-10-18'.to_date)
+
+puts 'Finished!'
