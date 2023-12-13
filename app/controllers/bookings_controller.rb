855c5eb57a95ac731468e0d1f4a5c5311978bda9
David Lau <david.kaimun.lau@gmail.com>
2023-12-13 10:55:38 +0100

    allow users to delete a booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -15,6 +15,23 @@ class BookingsController < ApplicationController
     end
   end
 
+  def update
+    listing = Listing.find(params[:listing_id])
+    booking = listing.bookings.find(params[:id])
+    if booking.update(booking_params)
+      render json: { booking: }, status: :ok
+    else
+      render json: { errors: booking.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    listing = Listing.find(params[:listing_id])
+    booking = listing.bookings.find(params[:id])
+    booking.destroy
+    head 204
+  end
+
   private
 
   def booking_params
